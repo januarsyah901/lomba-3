3f5eca2e3f363f8715c4dcfbadd825a43424b4c4
Fatih Fawwaz <fatihfwz87@gmail.com>
2023-05-15 11:08:14 +0700

    Penambahan Carousel di offer dan place

--- js/custom.js
+++ js/custom.js
@@ -19,3 +19,28 @@
     }
   });
 })(jQuery);
+
+// Carousel
+
+$(document).ready(function () {
+  const owl = $('.owl-carousel');
+  owl.owlCarousel({
+    dots:true,
+    loop: true,
+    margin: 30,
+    nav: true,
+    autoWidth: true,
+    
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
